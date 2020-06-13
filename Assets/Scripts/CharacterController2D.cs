f7ab844cae4b98d179e9be88e6a4404e6f5912f5
Giovanni Shibaki <giovannishibaki@gmail.com>
2020-06-13 12:58:07 -0300

    Scripts de Movimentação

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] public float forcaPulo = 400f; // Determina o quão forte é o pulo do personagem
+    // 400f por padrao	
+    [Range(0, .3f)] [SerializeField] public float m_MovementSmoothing = 0.05f; // Variável para suavizar a movimentação
+    // 0.05f por padrão
+
+
+    [SerializeField] private LayerMask m_WhatIsGround;   // Para determinar o que é chão para o personagem
+	[SerializeField] private Transform m_GroundCheck;    // Objeto para designar em que posição do personagem é o chao (no caso, no pé do personagem)
+	[SerializeField] private Transform m_CeilingCheck;   // Objeto para designar em que posição do personagem é o teto (no caso, a cabeça, topo do personagem)
+    [SerializeField] private bool m_AirControl = false;	// Verifica se o jogador pode se mover para esquerda ou direita enquanto estiver no ar (Pulando)
+    private bool olhandoDireita = true; // Serve para verificar a direção em que o jogador está olhando
+
+    private bool m_Grounded;            // Checar se o jogador está no chão ou não
+
+    private Rigidbody2D m_Rigidbody2D;
+
+    const float k_GroundedRadius = .2f; // Raio do objeto que determina onde é o pé do personagem
+	const float k_CeilingRadius = .2f; // Raio do objeto que determina onde é a cabeça do personagem
+
+	private Vector3 m_Velocity = Vector3.zero;
+
+
+    private void Awake()
+	{
+        m_Rigidbody2D = GetComponent<Rigidbody2D>(); //Cria o objeto para a movimentação do personagem
+    }
+
+    private void LateUpdate()
+	{
+        bool wasGrounded = m_Grounded;
+		m_Grounded = false;
+
+        // O jogador estará no chao se o objeto marcado para checar a posição do chão no personagem bate com alguma coisa associada ao chao
+		// Pode ser feita por layers, basta colocar os objetos na layer  correta
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				m_Grounded = true;
+				if (!wasGrounded)
+					OnLandEvent.Invoke();
+			}
+		}
+    }
+
+    // Recebe se o jogador está pulando ou apenas andando normalmente
+    public void Move(float movimento, bool pulando)
+	{
+        // Se pode se mover
+        if (m_AirControl)
+		{
+            // Move o personagem de acordo com sua velocidade atual
+			Vector3 velocidadeAtual = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+			// And then smoothing it out and applying it to the character
+			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, velocidadeAtual, ref m_Velocity, m_MovementSmoothing);
+
+			if (movimento > 0 && !olhandoDireita)
+			{
+				Flip(false); //virar para a direita
+			}
+			else if (movimento < 0 && olhandoDireita)
+			{
+				Flip(true); //virar para a esquerda
+			}
+        }
+
+    }
+
+    // Para mudanças de direção (direita - esquerda)
+    private void Flip(bool direcao)
+	{
+        olhandoDireita = !olhandoDireita; // Inverte o lado em que o jogador está olhando
+
+        // Mudando a escala de 1 para -1 ou ao contrário inverte a orientação do jogador na cena
+        Vector3 escala = transform.localScale;
+        if(direcao)
+        {
+            escala.x = -1;
+        }
+        else
+        {
+            escala.x = 1;
+        }
+
+        transform.localScale = escala;
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed;
+    // 40f por padrão
+    float horizontalMove;
+    bool jump = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        //animator.SetFloat("xSpeed", Mathf.Abs(horizontalMove)); // Caso tenha animação de andar
+
+        // Lembrar de setar os botões de pulo,e de andar direita e esquerda
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            //animator.SetBool("isJumping", true); // Caso tenha animação de pulo
+        }
+
+        if (Input.touchCount > 0)
+        {
+                Touch touch = Input.GetTouch(0);
+
+                float direcao = (touch.position.x) > (Screen.width / 2) ? 1 : -1; // Verifica a direção que o personagem vai andar
+                horizontalMove = direcao * runSpeed;
+                //animator.SetFloat("xSpeed", Mathf.Abs(horizontalMove)); // Caso tenha animação de andar
+
+            if ((Input.touchCount > 1))
+            {
+                jump = true;
+                //animator.SetBool("isJumping", true); // Caso tenha animação de pulo
+            }
+            if ((touch.position.y < Screen.width / 8))
+            {
+                crouch = true;
+            }
+        }
+
+    }
+
+    public void OnLanding()
+    {
+        //animator.SetBool("isJumping", false); //Desativa pulo CASO tenha animação
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+
+}
