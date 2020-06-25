578f5b78fc142d7f969768050a6441ba482c37d2
npdr <p.nieuwenhoff@usp.br>
2020-06-25 15:57:28 -0300

    organizado os scripts de acoes genericas

---
+++ Assets/Scripts/Acoes Genericas/BotaoPlataforma.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotaoPlataforma : MonoBehaviour
+{
+    public GameObject Player1;
+    public GameObject Player2;
+    private bool ColisaoP1 = false;
+    private bool ColisaoP2 = false;
+    public GameObject Plataforma;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        /* Caso o player1 colida com a porta*/
+        if(Object.ReferenceEquals(Player1, other.gameObject.transform.parent.gameObject))
+        {
+            ColisaoP1 = true;
+        }
+
+        if(Object.ReferenceEquals(Player2, other.gameObject.transform.parent.gameObject))
+        {
+            ColisaoP2 = true;
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        /* Caso o player1 saia da porta*/
+        if(Object.ReferenceEquals(Player1, other.gameObject.transform.parent.gameObject))
+        {
+            ColisaoP1 = false;
+        }
+
+        /* Caso o player2 saia da porta*/
+        if(Object.ReferenceEquals(Player2, other.gameObject.transform.parent.gameObject))
+        {
+            ColisaoP2 = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && ColisaoP1 == true)
+        {
+            /* Ativa a plataforma */
+            if(Plataforma.GetComponent<PlatformMovement>().ativada == false)
+                Plataforma.GetComponent<PlatformMovement>().ativada = true;
+            else Plataforma.GetComponent<PlatformMovement>().ativada = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.L) && ColisaoP2 == true)
+        {
+            if(Plataforma.GetComponent<PlatformMovement>().ativada == false)
+                Plataforma.GetComponent<PlatformMovement>().ativada = true;
+            else Plataforma.GetComponent<PlatformMovement>().ativada = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Acoes Genericas/EntraPorta.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntraPorta : MonoBehaviour
+{
+    public GameObject Player1;
+    public GameObject Player2;
+    public Transform TeleportPosition;
+    private bool ColisaoP1 = false;
+    private bool ColisaoP2 = false;
+    private string tipo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* Determina a tipo de peça que pode entrar na porta */
+        if(gameObject.tag == "PortaTriangulo")
+            tipo = "Triangulo";
+
+        else if(gameObject.tag == "PortaQuadrado")
+            tipo = "Quadrado";
+
+        else if(gameObject.tag == "PortaCirculo")
+            tipo = "Circulo";
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        /* Caso o player1 colida com a porta*/
+        if(Player1.transform.GetChild(0).tag == tipo)
+        {
+            if(Object.ReferenceEquals(Player1, other.gameObject.transform.parent.gameObject))
+            {
+                ColisaoP1 = true;
+            }
+        }
+
+
+        /* Caso o player2 colida com a porta*/
+        if(Player2.transform.GetChild(0).tag == tipo)
+        {
+            if(Object.ReferenceEquals(Player2, other.gameObject.transform.parent.gameObject))
+            {
+                ColisaoP2 = true;
+            }
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        /* Caso o player1 saia da porta*/
+        if(Object.ReferenceEquals(Player1, other.gameObject.transform.parent.gameObject))
+        {
+            ColisaoP1 = false;
+        }
+
+        /* Caso o player2 saia da porta*/
+        if(Object.ReferenceEquals(Player2, other.gameObject.transform.parent.gameObject))
+        {
+            ColisaoP2 = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && ColisaoP1 == true)
+        {
+            if(gameObject.tag == "PortaTriangulo" ||
+               gameObject.tag == "PortaQuadrado" ||
+               gameObject.tag == "PortaCirculo")
+            {
+                /* Teleporta o personagem */
+                Player1.transform.position = new Vector2 (TeleportPosition.position.x, TeleportPosition.position.y);
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.L) && ColisaoP2 == true)
+        {
+            if(gameObject.tag == "PortaTriangulo" ||
+               gameObject.tag == "PortaQuadrado" ||
+               gameObject.tag == "PortaCirculo")
+            {
+                /* Teleporta o personagem */
+                Player2.transform.position = new Vector2 (TeleportPosition.position.x, TeleportPosition.position.y);
+            }
+        }
+    }
+}
